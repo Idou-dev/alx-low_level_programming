8daeab2a44a0e72cf988bc1637213e688190e53b
idou-dev <idouyoussef87@gmail.com>
2023-05-28 06:57:20 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,7 +16,13 @@ int main(void)
 			printf("FizzBuzz ");
 		}
 		else if (i % 5 == 0)
-			printf("Buzz ");
+		{
+			printf("Buzz");
+			if (i < 100)
+			{
+				printf(" ");
+			}
+		}
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else
