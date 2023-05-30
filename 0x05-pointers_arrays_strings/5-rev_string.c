e65377f89902d09afa9b0bda4bcf4c08b54e9268
idou-dev <idouyoussef87@gmail.com>
2023-05-30 14:40:44 -0700

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,17 @@
 void rev_string(char *s)
 {
 	int l, i;
+	char c;
 
 	l = 0;
 	while (s[l] != '\0')
 	{
 		l++;
 	}
-	for (i = 0; i < l; i++)
+	for (i = l - 1; i >= l / 2; i--)
 	{
+		c = s[i];
 		s[i] = s[l - i - 1];
+		s[l - i - 1] = c;
 	}
 }
