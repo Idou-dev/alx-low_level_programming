4aea8fbbb7c860a4bf5063a344886427b6490627
idou-dev <idouyoussef87@gmail.com>
2023-05-30 13:57:36 -0700

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to a char
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int l, i;
+
+	l = 0;
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	for (i = 0; i < l; i++)
+	{
+		s[i - l - 1] = s[i];
+	}
+}
