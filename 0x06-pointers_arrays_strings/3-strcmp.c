aa0016170389af929378c04e83313c57d78e8886
idou-dev <idouyoussef87@gmail.com>
2023-06-01 16:10:42 -0700

    Strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first pointer to char
+ * @s2: second pointer to char
+ * Return: 0 if s1 and s2 are same random number if not
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, l, j;
+
+	l = 0;
+	while (s1[l] != '\0')
+	{
+		l++;
+	}
+	for (i = 0; i < l; i++)
+	{
+		if (s1[i] < s2[i])
+		{
+			return (-j);
+		}
+		else if (s1[i] > s2[i])
+		{
+			return (j);
+		}
+		j = 0;
+	}
+	return (j);
+}
