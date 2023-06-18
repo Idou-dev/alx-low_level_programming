8558bc6543b38992e45866cea15ed96a349ea6ad
idou-dev <idouyoussef87@gmail.com>
2023-06-18 08:57:03 -0700

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,44 +11,35 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i, l1 = 0, l2 = 0;
+	unsigned int i = 0, l = 0, j = 0, k = 0;
 
-	if (s1 == NULL && s2 == NULL)
-		s = '\0';
-	if (s1 != NULL)
-	{
-		while (s1[l1] != '\0')
-			l1++;
-	}
-	if (s2 != NULL)
-	{
-		while (s2[l2] != '\0')
-			l2++;
-	}
-	if (n < l2)
-		s = malloc((l1 + n) * sizeof(char) + 1);
-	if (n >= l2)
-		s = malloc((l1 + l2) * sizeof(char) + 1);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[k])
+		k++;
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+	s = malloc(l * sizeof(char) + 1);
 	if (s == NULL)
 		return (NULL);
-	if (s1 != NULL)
+	k = 0;
+	while (j < l)
 	{
-		for (i = 0; i < l1; i++)
-			s[i] = s1[i];
-	}
-	if (s2 != NULL)
-	{
-		if (n >= l2)
+		if (j <= i)
+			s[j] = s1[j];
+		if (j >= i)
 		{
-			for (i = 0; i <= l2; i++)
-				s[l1 + i] = s2[i];
+			s[j] = s2[k];
+			k++;
 		}
-		else
-		{
-			for (i = 0; i < n; i++)
-				s[l1 + i] = s2[i];
-			s[l1 + i] = '\0';
-		}
+		j++;
 	}
+	s[j] = '\0';
 	return (s);
 }
