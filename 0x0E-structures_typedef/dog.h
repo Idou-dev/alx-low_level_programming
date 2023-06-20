0bef9d044065243053511c721bc7bd17fa8af51b
idou-dev <idouyoussef87@gmail.com>
2023-06-20 11:01:18 -0700

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type
+ * @name: first element pointer to char
+ * @age: second element a float
+ * @owner: third element pointer to char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
