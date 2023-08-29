3b3f84f4fb615dd3b3354caf29359b00b2a8b217
idou-dev <idouyoussef87@gmail.com>
2023-08-29 16:54:27 -0700

    frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: first node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *curr = *head;
+	listint_t *aux;
+
+	while (curr != NULL)
+	{
+		aux = curr;
+		curr = curr->next;
+		free(aux);
+	}
+	head = NULL;
+}
