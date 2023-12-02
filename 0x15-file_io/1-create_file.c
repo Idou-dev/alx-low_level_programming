6614ae923cdf9675f9acc01115a2e25fdb7cf636
idou-dev <idouyoussef87@gmail.com>
2023-12-02 05:10:00 -0800

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,16 +25,17 @@ int _strlen(char *s)
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w;
+	int fd, w, nlet;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
 		text_content = "";
-	w = write(fd, text_content, _strlen(text_content));
+	nlet = _strlen(text_content);
+	w = write(fd, text_content, nlet);
 	if (w == -1)
 		return (-1);
 	close(fd);
