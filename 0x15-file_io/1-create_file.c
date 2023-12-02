e620ff0701415714da7335cc12bb011760b32896
idou-dev <idouyoussef87@gmail.com>
2023-12-02 06:11:35 -0800

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,20 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - returns a length of a string
- * @s: the string
- * Return: the length of the string
-
-int _strlen(char *s)
-{
-	int l;
-
-	while (s[l] != '\0')
-		l++;
-	return (l);
-}*/
-
-/**
  * create_file - creates a file
  * @filename: name of the file to create
  * @text_content: NULL terminated string to write
@@ -33,7 +19,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 		text_content = "";
-	for (nlet = 0; text_content[nlet]; nlet++);
+	for (nlet = 0; text_content[nlet]; nlet++)
+		;
 	w = write(fd, text_content, nlet);
 	if (w == -1)
 		return (-1);
