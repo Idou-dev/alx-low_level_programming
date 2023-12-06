328a61f44bebe4baa0ac03fb4a86a324e23d3a5e
idou-dev <idouyoussef87@gmail.com>
2023-12-05 17:20:52 -0800

    copies content of a file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
+ * check_stat - checks file if it is open or closed
+ * @n: an integer
+ * @s: name of file
+ * @fd: file descriptor value
+ */
+
+void check_stat(int n, int fd, char *s)
+{
+	if (n == 1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", s);
+		exit(98);
+	}
+	if (n == 2)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
+		exit(99);
+	}
+	if (n == 3)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd);
+		exit(100);
+	}
+}
+/**
  * main - the main function
  * @ac: argument count
  * @av: argument vector
@@ -9,7 +34,7 @@
 
 int main(int ac, char **av)
 {
-	int fd, w;
+	int fd, fd1, w, n_read = 1024;
 	char buf[READ_BUF_SIZE];
 
 	if (ac != 3)
@@ -17,28 +42,26 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	fd1 = open(av[1], O_WRONLY);
+	if (fd1 == -1)
+		check_stat(1, -1, av[1]);
 	fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd == -1)
+		check_stat(2, -1, av[2]);
+	while (n_read == 1024)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
+		n_read = read(fd1, buf, sizeof(buf));
+		if (w == -1)
+			check_stat(1, -1, av[1]);
+		w = write(fd, buf, n_read);
+		if (w == -1)
+			check_stat(2, -1, av[2]);
 	}
-	w = read(fd, &buf[0], *av[1]);
+	w = close(fd1);
 	if (w == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	w = write(fd, buf, w);
-	if (w == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+		check_stat(3, fd1, av[1]);
 	w = close(fd);
 	if (w == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-	}
+		check_stat(3, fd, av[2]);
 	return (0);
 }
